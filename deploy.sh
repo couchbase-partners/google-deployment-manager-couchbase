f6f769335ab461d7a2e3e56046dbe693bf244bbc
benofben <ben.lackey@outlook.com>
2017-05-04 15:25:41 -0700

    so much for that bright idea with the params

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 PARAMETERS_FILE=$1
 DEPLOYMENT_NAME=$2
 
-gcloud deployment-manager deployments create ${DEPLOYMENT_NAME} --config parameters/${PARAMETERS_FILE}.yaml
+gcloud deployment-manager deployments create ${DEPLOYMENT_NAME} --config parameters.${PARAMETERS_FILE}.yaml
