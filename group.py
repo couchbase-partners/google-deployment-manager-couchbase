47b399a3e42b9a7e18d7348d6021963c6255170b
benofben <ben.lackey@outlook.com>
2017-05-16 16:59:20 -0600

    starting work on config

--- group.py
+++ group.py
@@ -113,8 +113,80 @@ vm.swappiness = 0" >> /etc/sysctl.conf
 '''
 
 configure = '''
+echo "Running configure.sh"
+
+echo "Using the settings:"
+echo adminUsername \'$adminUsername\'
+echo adminPassword \'$adminPassword\'
+
+rallyPrivateDNS=''
+nodePrivateDNS=`curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance`
+
+cd /opt/couchbase/bin/
+
+echo "Running couchbase-cli node-init"
+./couchbase-cli node-init \
+  --cluster=$nodePrivateDNS \
+  --node-init-hostname=$nodePrivateDNS \
+  --node-init-data-path=/mnt/datadisk/data \
+  --node-init-index-path=/mnt/datadisk/index \
+  --user=$adminUsername \
+  --pass=$adminPassword
+
+if [[ $rallyPrivateDNS == $nodePrivateDNS ]]
+then
+  totalRAM=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+  dataRAM=$((50 * $totalRAM / 100000))
+  indexRAM=$((15 * $totalRAM / 100000))
+
+  echo "Running couchbase-cli cluster-init"
+  ./couchbase-cli cluster-init \
+    --cluster=$nodePrivateDNS \
+    --cluster-ramsize=$dataRAM \
+    --cluster-index-ramsize=$indexRAM \
+    --cluster-username=$adminUsername \
+    --cluster-password=$adminPassword \
+    --services=data,index,query,fts
+else
+  echo "Running couchbase-cli server-add"
+  output=""
+  while [[ $output != "Server $nodePrivateDNS:8091 added" && ! $output =~ "Node is already part of cluster." ]]
+  do
+    vm0PrivateDNS=`host vm0 | awk '{print $1}'`
+    output=`./couchbase-cli server-add \
+      --cluster=$rallyPrivateDNS \
+      --user=$adminUsername \
+      --pass=$adminPassword \
+      --server-add=$nodePrivateDNS \
+      --server-add-username=$adminUsername \
+      --server-add-password=$adminPassword \
+      --services=data,index,query,fts`
+    echo server-add output \'$output\'
+    sleep 10
+  done
+
+  echo "Running couchbase-cli rebalance"
+  output=""
+  while [[ ! $output =~ "SUCCESS" ]]
+  do
+    output=`./couchbase-cli rebalance \
+      --cluster=$rallyPrivateDNS \
+      --user=$adminUsername \
+      --pass=$adminPassword`
+    echo rebalance output \'$output\'
+    sleep 10
+  done
+
+fi
 '''
 
 def GenerateStartupScript():
-    script = install + configure
+    script = install
+
+    script+='''
+    adminUsername='couchbase'
+    adminPassword='foo123!'
+    '''
+
+    script+=configure
     return script
