c95b61417204a0e5dfff444db251297a38be61ea
benofben <ben.lackey@outlook.com>
2017-05-17 11:47:30 -0600

    tab/space reformat, function refactor

--- couchbase.py
+++ couchbase.py
@@ -3,39 +3,22 @@ def GenerateConfig(context):
     config['resources'] = []
     config['outputs'] = []
 
-    deployment = context.env['deployment']
-    couchbaseUsername=context.properties['couchbaseUsername']
-    couchbasePassword=context.properties['couchbasePassword']
-
     for cluster in context.properties['clusters']:
-        clusterName = cluster['cluster']
-        region = cluster['region']
         for group in cluster['groups']:
-            groupName = group['group']
-            diskSize = group['diskSize']
-            machineCount = group['machineCount']
-            machineType = group['machineType']
-
-            if group.hasKey('services'):
-                services = group['services']
-                groupJSON=GenerateGroup(deployment, clusterName, region, groupName, diskSize, machineCount, machineType)
-                config['resources'].append(groupJSON)
-            else:
-                mobileServices = group['mobileServices']
-
+            groupJSON = {
+                'name': context.env['deployment'] + '-' + cluster['cluster'] + '-' + group['group'],
+                'type': 'group.py',
+                'properties': {
+                    'couchbaseUsername': context.properties['couchbaseUsername'],
+                    'couchbasePassword': context.properties['couchbasePassword'],
+                    'clusterName': cluster['cluster'],
+                    'region': cluster['region'],
+                    'groupName': group['group'],
+                    'diskSize': group['diskSize'],
+                    'machineCount': group['machineCount'],
+                    'machineType': group['machineType'],
+                    'services': group['services']
+                }
+            }
+            config['resources'].append(groupJSON)
     return config
-
-def GenerateGroup(deployment, clusterName, region, groupName, diskSize, machineCount, machineType):
-    groupJSON = {
-        'name': deployment + '-' + region + '-' + groupName,
-        'type': 'group.py',
-        'properties': {
-            'clusterName': clusterName,
-            'region': region,
-            'groupName': groupName,
-            'diskSize': diskSize,
-            'machineCount': machineCount,
-            'machineType': machineType,
-        }
-    }
-    return groupJSON
--- group.py
+++ group.py
@@ -1,66 +1,66 @@
-URL_BASE = 'https://www.googleapis.com/compute/v1/projects/'
-
 def GenerateConfig(context):
-  deployment = context.env['deployment']
-  clusterName = context.properties['clusterName']
-  region = context.properties['region']
-  groupName = context.properties['groupName']
-  machineCount = context.properties['machineCount']
-  machineType = context.properties['machineType']
-  diskSize = context.properties['diskSize']
+    URL_BASE = 'https://www.googleapis.com/compute/v1/projects/'
 
-  items = []
-  items.append({'key':'startup-script', 'value':GenerateStartupScript()})
-  metadata = {'items': items}
+    items = []
+    items.append({'key':'startup-script', 'value':GenerateStartupScript()})
+    metadata = {'items': items}
 
-  it_name = deployment + '-' + clusterName + '-' + groupName + '-it'
-  it = {
-    'name': it_name,
-    'type': 'compute.v1.instanceTemplate',
-    'properties': {
-      'properties': {
-        'machineType': machineType,
-        'networkInterfaces': [{
-          'network': URL_BASE + context.env['project'] + '/global/networks/default',
-          'accessConfigs': [{
-            'name': 'External NAT',
-            'type': 'ONE_TO_ONE_NAT'
-          }]
-        }],
-        'disks': [{
-          'deviceName': 'boot',
-          'type': 'PERSISTENT',
-          'boot': True,
-          'autoDelete': True,
-          'initializeParams': {
-            'sourceImage': URL_BASE + 'ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20170424'
-          },
-          'diskType': 'pd-ssd',
-          'diskSizeGb': diskSize
-        }],
-        'metadata': metadata,
-      }
+    it_name = context.env['deployment'] + '-' + context.properties['clusterName'] + '-' + context.properties['groupName'] + '-it'
+    it = {
+        'name': it_name,
+        'type': 'compute.v1.instanceTemplate',
+        'properties': {
+            'properties': {
+                'machineType': context.properties['machineType'],
+                'networkInterfaces': [{
+                    'network': URL_BASE + context.env['project'] + '/global/networks/default',
+                    'accessConfigs': [{
+                        'name': 'External NAT',
+                        'type': 'ONE_TO_ONE_NAT'
+                    }]
+                }],
+                'disks': [{
+                    'deviceName': 'boot',
+                    'type': 'PERSISTENT',
+                    'boot': True,
+                    'autoDelete': True,
+                    'initializeParams': {
+                        'sourceImage': URL_BASE + 'ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20170424'
+                    },
+                    'diskType': 'pd-ssd',
+                    'diskSizeGb': context.properties['diskSize']
+                }],
+                'metadata': metadata,
+            }
+        }
     }
-  }
 
-  igm = {
-    'name': deployment + '-' + clusterName + '-' + groupName + '-igm',
-    'type': 'compute.v1.regionInstanceGroupManager',
-    'properties': {
-      'region': region,
-      'baseInstanceName': deployment + '-' + clusterName + '-' + groupName + '-instance',
-      'instanceTemplate': '$(ref.' + it_name + '.selfLink)',
-      'targetSize': machineCount,
-      'autoHealingPolicies': [{
-        'initialDelaySec': 60
-      }]
+    igm = {
+        'name': context.env['deployment'] + '-' + context.properties['clusterName'] + '-' + context.properties['groupName'] + '-igm',
+        'type': 'compute.v1.regionInstanceGroupManager',
+        'properties': {
+            'region': context.properties['region'],
+            'baseInstanceName': context.env['deployment'] + '-' + context.properties['clusterName'] + '-' + context.properties['groupName'] + '-instance',
+            'instanceTemplate': '$(ref.' + it_name + '.selfLink)',
+            'targetSize': context.properties['machineCount'],
+            'autoHealingPolicies': [{
+                'initialDelaySec': 60
+            }]
+        }
     }
-  }
 
-  resources = []
-  resources.append(it)
-  resources.append(igm)
-  return {'resources': resources}
+    resources = []
+    resources.append(it)
+    resources.append(igm)
+    return {'resources': resources}
+
+def GenerateStartupScript():
+    script = install
+    return script
+
+#######################################################
+################### Startup Scripts ###################
+#######################################################
 
 install='''
 #!/usr/bin/env bash
@@ -179,14 +179,3 @@ else
 
 fi
 '''
-
-def GenerateStartupScript():
-    script = install
-
-    script+='''
-    adminUsername='couchbase'
-    adminPassword='foo123!'
-    '''
-
-    script+=configure
-    return script
