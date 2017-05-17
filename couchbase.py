e78a76a4af97877796dca0b13c8f782bee5de095
benofben <ben.lackey@outlook.com>
2017-05-17 11:12:28 -0600

    tabs and spaces!

--- couchbase.py
+++ couchbase.py
@@ -1,38 +1,41 @@
 def GenerateConfig(context):
-  config={}
-  config['resources'] = []
-  config['outputs'] = []
+    config={}
+    config['resources'] = []
+    config['outputs'] = []
 
-  deployment = context.env['deployment']
-  couchbaseUsername=context.properties['couchbaseUsername']
-  couchbasePassword=context.properties['couchbasePassword']
+    deployment = context.env['deployment']
+    couchbaseUsername=context.properties['couchbaseUsername']
+    couchbasePassword=context.properties['couchbasePassword']
 
-  for cluster in context.properties['clusters']:
-    clusterName = cluster['cluster']
-    region = cluster['region']
-    for group in cluster['groups']:
-      groupName = group['group']
-      diskSize = group['diskSize']
-      machineCount = group['machineCount']
-      machineType = group['machineType']
-      services = group['services']
+    for cluster in context.properties['clusters']:
+        clusterName = cluster['cluster']
+        region = cluster['region']
+        for group in cluster['groups']:
+            groupName = group['group']
+            diskSize = group['diskSize']
+            machineCount = group['machineCount']
+            machineType = group['machineType']
 
-      groupJSON=GenerateGroup(deployment, clusterName, region, groupName, diskSize, machineCount, machineType)
-      config['resources'].append(groupJSON)
+            if group.hasKey('services'):
+                services = group['services']
+                groupJSON=GenerateGroup(deployment, clusterName, region, groupName, diskSize, machineCount, machineType)
+                config['resources'].append(groupJSON)
+            else:
+                mobileServices = group['mobileServices']
 
-  return config
+    return config
 
 def GenerateGroup(deployment, clusterName, region, groupName, diskSize, machineCount, machineType):
-  groupJSON = {
-    'name': deployment + '-' + region + '-' + groupName,
-    'type': 'group.py',
-    'properties': {
-      'clusterName': clusterName,
-      'region': region,
-      'groupName': groupName,
-      'diskSize': diskSize,
-      'machineCount': machineCount,
-      'machineType': machineType,
+    groupJSON = {
+        'name': deployment + '-' + region + '-' + groupName,
+        'type': 'group.py',
+        'properties': {
+            'clusterName': clusterName,
+            'region': region,
+            'groupName': groupName,
+            'diskSize': diskSize,
+            'machineCount': machineCount,
+            'machineType': machineType,
+        }
     }
-  }
-  return groupJSON
+    return groupJSON
