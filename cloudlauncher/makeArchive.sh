ea0486ac20be26ac1c82b5ec34bc93e7db72bbce
benofben <ben.lackey@outlook.com>
2017-08-24 16:47:30 -0700

    import and get ready for rename

--- cloudlauncher/makeArchive.sh
+++ cloudlauncher/makeArchive.sh
@@ -6,8 +6,11 @@ cp -r ../simple/*.py tmp
 cp -r ../simple/scripts tmp
 
 cp -r resources tmp
+#cp couchbase.py tmp
+cp couchbase.jinja tmp
+cp couchbase.jinja.display tmp
+cp couchbase.jinja.schema tmp
 cp c2d_deployment_configuration.json tmp
-cp couchbase.jinja* tmp
 cp test_config.yaml tmp
 
 zip -r -X archive.zip tmp
