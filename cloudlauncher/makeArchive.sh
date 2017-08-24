a51b05fa2b6a9f6044f20fb5cf4e27cb1873cb9a
benofben <ben.lackey@outlook.com>
2017-08-24 11:01:22 -0700

    make archive work

--- cloudlauncher/makeArchive.sh
+++ cloudlauncher/makeArchive.sh
@@ -1,5 +1,14 @@
-# copy the scripts to tmp
-# ./ to tmp
-# dm to tmp
-# zip
-# remove tmp
+#!/bin/sh
+
+#mkdir tmp
+
+#cp -r ../simple/*.py tmp
+cp -r ../simple/scripts tmp
+
+cp -r resources tmp
+cp c2d_deployment_configuration.json tmp
+cp couchbase.jinja* tmp
+cp test_config.yaml tmp
+
+#rm -rf tmp
+zip -r -X archive.zip tmp
