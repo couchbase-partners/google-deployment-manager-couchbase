2a7b68e97adcfd95a3975204dfdfc63840f47bd5
benofben <ben.lackey@outlook.com>
2017-08-24 11:21:02 -0700

    use latest from simple

--- cloudlauncher/makeArchive.sh
+++ cloudlauncher/makeArchive.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#mkdir tmp
+mkdir tmp
 
-#cp -r ../simple/*.py tmp
+cp -r ../simple/*.py tmp
 cp -r ../simple/scripts tmp
 
 cp -r resources tmp
@@ -10,5 +10,5 @@ cp c2d_deployment_configuration.json tmp
 cp couchbase.jinja* tmp
 cp test_config.yaml tmp
 
-#rm -rf tmp
 zip -r -X archive.zip tmp
+rm -rf tmp
