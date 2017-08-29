3a8e53eddc2f12d8891fc9a41995558d3bb9e40b
benofben <ben.lackey@outlook.com>
2017-08-29 16:29:36 -0700

    drop comment

--- simple/server.sh
+++ simple/server.sh
@@ -45,9 +45,6 @@ vm.swappiness = 0" >> /etc/sysctl.conf
 #######################################################
 
 echo "Installing Couchbase Server..."
-
-# Using these instructions
-# https://developer.couchbase.com/documentation/server/4.6/install/ubuntu-debian-install.html
 wget http://packages.couchbase.com/releases/4.6.2/couchbase-server-enterprise_4.6.2-ubuntu14.04_amd64.deb
 dpkg -i couchbase-server-enterprise_4.6.2-ubuntu14.04_amd64.deb
 apt-get update
