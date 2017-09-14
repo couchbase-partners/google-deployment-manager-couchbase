0e65e82975098d82df7bed7df423618aba0da124
benofben <ben.lackey@outlook.com>
2017-09-14 08:20:08 -0700

    loop over list of regions

--- cloudlauncher/couchbase.py
+++ cloudlauncher/couchbase.py
@@ -46,26 +46,20 @@ def GetClusters(context):
 
 def GetRegionsList(context):
     regions = []
-    if context.properties['us-central1']:
-        regions.append('us-central1')
-    if context.properties['us-west1']:
-        regions.append('us-west1')
-    if context.properties['us-east1']:
-        regions.append('us-east1')
-    if context.properties['us-east4']:
-        regions.append('us-east4')
-    if context.properties['europe-west1']:
-        regions.append('europe-west1')
-    if context.properties['europe-west2']:
-        regions.append('europe-west2')
-    if context.properties['europe-west3']:
-        regions.append('europe-west3')
-    if context.properties['asia-southeast1']:
-        regions.append('asia-southeast1')
-    if context.properties['asia-east1']:
-        regions.append('asia-east1')
-    if context.properties['asia-northeast1']:
-        regions.append('asia-northeast1')
-    if context.properties['australia-southeast1']:
-        regions.append('australia-southeast1')
+    availableRegions = [
+        'us-central1',
+        'us-west1',
+        'us-east1',
+        'us-east4',
+        'europe-west1',
+        'europe-west2',
+        'europe-west3',
+        'asia-southeast1',
+        'asia-east1',
+        'asia-northeast1',
+        'australia-southeast1'
+    ]
+    for region in availableRegions:
+        if context.properties[region]:
+            regions.append(region)
     return regions
