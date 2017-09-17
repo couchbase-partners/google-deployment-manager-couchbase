42691f40100ec7316e8ddfda5327a45f8bd488d9
benofben <ben.lackey@outlook.com>
2017-09-16 22:35:34 -0700

    rename

---
+++ cloudlauncher/makeArchives.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+function makeArchive()
+{
+  license=$1
+  rm archive-${license}.zip
+  mkdir tmp
+
+  cp couchbase-${license}.py tmp/couchbase.py
+  cp couchbase.py.display tmp
+  cp couchbase.py.schema tmp
+  cp c2d_deployment_configuration.json tmp
+  cp test_config.yaml tmp
+
+  cp ../simple/deployment.py tmp
+  cp ../simple/cluster.py tmp
+  cp ../simple/group.py tmp
+  cp ../simple/server.sh tmp
+  cp ../simple/syncGateway.sh tmp
+
+  cp -r resources tmp
+
+  zip -r -X archive-${license}.zip tmp
+  rm -rf tmp
+}
+
+makeArchive byol
+makeArchive hourly-pricing
