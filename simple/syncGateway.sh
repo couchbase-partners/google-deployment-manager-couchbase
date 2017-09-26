958b94905309ea5278e1be28f967b6be970ddd83
benofben <ben.lackey@outlook.com>
2017-09-26 13:03:20 -0700

    I think sync gateway will work now too.  time to try!

--- simple/syncGateway.sh
+++ simple/syncGateway.sh
@@ -28,35 +28,18 @@ apt-get -y install jq
 
 ACCESS_TOKEN=$(curl -s -H "Metadata-Flavor:Google" http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token | awk -F\" '{ print $4 }')
 PROJECT_ID=$(curl -s -H "Metadata-Flavor:Google" http://metadata.google.internal/computeMetadata/v1/project/project-id)
-CONFIG=${DEPLOYMENT}-${CLUSTER}-runtimeconfig
+CONFIG=${DEPLOYMENT}-runtimeconfig
 
-nodePrivateDNS=`curl -s http://metadata/computeMetadata/v1beta1/instance/hostname`
-echo nodePrivateDNS: ${nodePrivateDNS}
-
-# Get nodeCount from runtimeconfig
-nodeCount=$(curl -s -H "Authorization":"Bearer ${ACCESS_TOKEN}" \
-  https://runtimeconfig.googleapis.com/v1beta1/projects/${PROJECT_ID}/configs/${CONFIG}/variables/nodeCount \
-  | jq ".text" \
-  | sed 's/"//g')
-echo nodeCount: ${nodeCount}
-
-liveNodeCount=0
-while [[ $liveNodeCount -lt $nodeCount ]]
+rallyPrivateDNS=null
+while [[ $rallyPrivateDNS == "null" ]]
 do
-  # Get number of nodes currently in runtime config
-  liveNodeCount=$(curl -s -H "Authorization":"Bearer ${ACCESS_TOKEN}" \
-    https://runtimeconfig.googleapis.com/v1beta1/projects/${PROJECT_ID}/configs/${CONFIG}/variables/?filter=projects%2F${PROJECT_ID}%2Fconfigs%2F${CONFIG}%2Fvariables%2FnodeList \
-    | jq ".variables | length")
-  echo liveNodeCount: ${liveNodeCount}
   sleep 10
+  rallyPrivateDNS=$(curl -s -H "Authorization":"Bearer ${ACCESS_TOKEN}" \
+    https://runtimeconfig.googleapis.com/v1beta1/projects/${PROJECT_ID}/configs/${CONFIG}/variables/${CLUSTER}/rallyPrivateDNS \
+    | jq ".text" \
+    | sed 's/"//g')
+  echo rallyPrivateDNS: ${rallyPrivateDNS}
 done
-
-serverDNS=$(curl -s -H "Authorization":"Bearer ${ACCESS_TOKEN}" \
-  https://runtimeconfig.googleapis.com/v1beta1/projects/${PROJECT_ID}/configs/${CONFIG}/variables/?filter=projects%2F${PROJECT_ID}%2Fconfigs%2F${CONFIG}%2Fvariables%2FnodeList\&returnValues=True \
-  | jq ".variables | sort_by(.text)" \
-  | jq ".[0].text" \
-  | sed 's/"//g')
-echo serverDNS: ${serverDNS}
 
 file="/home/sync_gateway/sync_gateway.json"
 echo '
@@ -66,7 +49,7 @@ echo '
   "log": ["*"],
   "databases": {
     "database": {
-      "server": "http://'${serverDNS}':8091",
+      "server": "http://'${rallyPrivateDNS}':8091",
       "bucket": "sync_gateway",
       "users": {
         "GUEST": { "disabled": false, "admin_channels": ["*"] }
