ece3dfb95a060b15a45acd1eb9652c78f09c40fc
benofben <ben.lackey@outlook.com>
2017-10-02 21:26:03 -0700

    fix mk archive

--- cloudlauncher/makeArchives.sh
+++ cloudlauncher/makeArchives.sh
@@ -7,7 +7,6 @@ function makeArchive()
   mkdir tmp
 
   cp couchbase-${license}.py tmp/couchbase.py
-  cp password.py tmp
   cp couchbase.py.display tmp
   cp couchbase.py.schema tmp
   cp c2d_deployment_configuration.json tmp
